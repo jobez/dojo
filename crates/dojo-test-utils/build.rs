1b485f7f472e01df9f2d02c12921efbaee0195b1
johann makram salib bestowrous <jmsb@johanns-MBP.lan>
2023-06-26 16:40:38 -0700

    experimental lining up to require as a dependency

--- crates/dojo-test-utils/build.rs
+++ crates/dojo-test-utils/build.rs
@@ -9,25 +9,25 @@ use scarb::ops;
 use scarb::ui::Verbosity;
 
 fn main() {
-    let mut compilers = CompilerRepository::empty();
-    compilers.add(Box::new(DojoCompiler)).unwrap();
+    // let mut compilers = CompilerRepository::empty();
+    // compilers.add(Box::new(DojoCompiler)).unwrap();
 
-    let cairo_plugins = CairoPluginRepository::new();
+    // let cairo_plugins = CairoPluginRepository::new();
 
-    let cache_dir = assert_fs::TempDir::new().unwrap();
-    let config_dir = assert_fs::TempDir::new().unwrap();
+    // let cache_dir = assert_fs::TempDir::new().unwrap();
+    // let config_dir = assert_fs::TempDir::new().unwrap();
 
-    let path = Utf8PathBuf::from_path_buf("../../examples/ecs/Scarb.toml".into()).unwrap();
-    let config = Config::builder(path.canonicalize_utf8().unwrap())
-        .global_cache_dir_override(Some(Utf8Path::from_path(cache_dir.path()).unwrap()))
-        .global_config_dir_override(Some(Utf8Path::from_path(config_dir.path()).unwrap()))
-        .ui_verbosity(Verbosity::Verbose)
-        .log_filter_directive(env::var_os("SCARB_LOG"))
-        .compilers(compilers)
-        .cairo_plugins(cairo_plugins.into())
-        .build()
-        .unwrap();
+    // let path = Utf8PathBuf::from_path_buf("../../examples/ecs/Scarb.toml".into()).unwrap();
+    // let config = Config::builder(path.canonicalize_utf8().unwrap())
+    //     .global_cache_dir_override(Some(Utf8Path::from_path(cache_dir.path()).unwrap()))
+    //     .global_config_dir_override(Some(Utf8Path::from_path(config_dir.path()).unwrap()))
+    //     .ui_verbosity(Verbosity::Verbose)
+    //     .log_filter_directive(env::var_os("SCARB_LOG"))
+    //     .compilers(compilers)
+    //     .cairo_plugins(cairo_plugins.into())
+    //     .build()
+    //     .unwrap();
 
-    let ws = ops::read_workspace(config.manifest_path(), &config).unwrap();
-    ops::compile(&ws).unwrap();
+    // let ws = ops::read_workspace(config.manifest_path(), &config).unwrap();
+    // ops::compile(&ws).unwrap();
 }
